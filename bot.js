a2db4722488427968f3f20f1dfb7871efa886e73
shahzodbekme <myshahzodbek@gmail.com>
2026-03-16 22:11:18 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,10 +27,10 @@ async function sendPrices() {
     const { data } = await axios.get(url);
 
 const message = 
-      `<i>₿ BTC:</i> <b>$${data.bitcoin.usd}</b> (<i>${data.bitcoin.usd_24h_change.toFixed(2)}%</i>)\n` +
-      `<i>⟠ ETH:</i> <b>$${data.ethereum.usd}</b> (<i>${data.ethereum.usd_24h_change.toFixed(2)}%</i>)\n` +
-      `<i>◎ SOL:</i> <b>$${data.solana.usd}</b> (<i>${data.solana.usd_24h_change.toFixed(2)}%</i>)\n\n` +
-      `<u>⏱ Oxirgi yangilanish: ${new Date().toLocaleTimeString('uz-UZ')}</u>`;
+      `₿ BTC: $${data.bitcoin.usd} (${data.bitcoin.usd_24h_change.toFixed(2)}%)\n\n` +
+      `⟠ ETH: $${data.ethereum.usd} (${data.ethereum.usd_24h_change.toFixed(2)}%)\n\n` +
+      `◎ SOL: $${data.solana.usd} (${data.solana.usd_24h_change.toFixed(2)}%)\n\n` +
+      `⏱ ${new Date().toLocaleTimeString('uz-UZ')}`;
     
     await bot.sendMessage(CHANNEL, message);
     console.log("Xabar yuborildi!");
